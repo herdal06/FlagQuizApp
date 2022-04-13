8aab68265d485bc7862aebab46ac06568e44db43
herdal06 <h.erdal721@gmail.com>
2022-04-14 02:45:00 +0300

    Get random 3 wrong answers method

--- app/src/main/java/com/herdal/flagquizapp/FlagDao.kt
+++ app/src/main/java/com/herdal/flagquizapp/FlagDao.kt
@@ -21,4 +21,21 @@ class FlagDao {
         }
         return flagList
     }
+    // get random 5 wrong answers
+    @SuppressLint("Range")
+    fun getRandom5WrongAnswers(dbHelper: DatabaseHelper, flagId: Int) : ArrayList<Flag> {
+        val flagList = ArrayList<Flag>()
+        val db = dbHelper.writableDatabase
+        val c = db.rawQuery("SELECT * FROM flags WHERE id != $flagId ORDER BY RANDOM() LIMIT 3",null)
+
+        while(c.moveToNext()) {
+            val flag = Flag(
+                c.getInt(c.getColumnIndex("id"))
+                ,c.getString(c.getColumnIndex("name"))
+                ,c.getString(c.getColumnIndex("image"))
+            )
+            flagList.add(flag)
+        }
+        return flagList
+    }
 }
