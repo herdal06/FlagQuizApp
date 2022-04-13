77f0045f10e7beb9683273c665b8bcf87ab21c39
herdal06 <h.erdal721@gmail.com>
2022-04-14 02:23:15 +0300

    Create Database and Database Helper

---
+++ app/src/main/java/com/herdal/flagquizapp/DatabaseHelper.kt
@@ -0,0 +1,16 @@
+package com.herdal.flagquizapp
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context,"flagquiz.sqlite",null,1) {
+    override fun onCreate(db: SQLiteDatabase?) { // table definition
+        db?.execSQL("CREATE TABLE IF NOT EXISTS 'flags' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' TEXT, 'image' TEXT)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS flags")
+        onCreate(db)
+    }
+}
